a53b1e1d394e70ef72c1ee1eaf65b0b17feac858
Hayden Hung Hoang <hayden.hhoang@gmail.com>
2026-02-25 23:48:52 +0700

    fix package import paths

--- packages/typesense-fumadocs-adapter/CHANGELOG.md
+++ packages/typesense-fumadocs-adapter/CHANGELOG.md
@@ -1,5 +1,11 @@
 # typesense-fumadocs-adapter
 
+## 0.1.2
+
+### Patch Changes
+
+- fix import paths
+
 ## 0.1.1
 
 ### Patch Changes
--- packages/typesense-fumadocs-adapter/tsdown.config.ts
+++ packages/typesense-fumadocs-adapter/tsdown.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   entry: ['./index.ts', './client/index.ts'],
+  format: ['esm', 'cjs'],
+  clean: true,
   external: ['typesense', 'fumadocs-core', 'fumadocs-ui', 'react', 'react-dom'],
   dts: true,
   target: 'ES2020',
